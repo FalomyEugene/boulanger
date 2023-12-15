3405989d2503f61a2b0431285502f8f985d63800
FalomyEugene <93395642+FalomyEugene@users.noreply.github.com>
2023-12-15 11:28:23 -0600

    Update boulanger_app.py
    Code to add immages

--- boulanger_app.py
+++ boulanger_app.py
@@ -1,3 +1,12 @@
-import streamlit
+import streamlit as st
+import pandas as pd
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
-streamlit.title('Falomy Boulanger is in the oven')
+# Adding a title for the app
+
+st.title('Falomy Boulanger is in the oven')
+
+import streamlit as st
+st.image('sunrise.jpg', caption='Sunrise by the mountains')
