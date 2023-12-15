613833a115d9df4a4d35fff4110d198501735f70
FalomyEugene <93395642+FalomyEugene@users.noreply.github.com>
2023-12-15 12:01:17 -0600

    Update boulanger_app.py
    Check the title

--- boulanger_app.py
+++ boulanger_app.py
@@ -5,10 +5,11 @@ from urllib.error import URLError
 
 # Adding a title for the app
 
-st.title('🍞 Falomy Boulanger; Où la farine et le sucre dansent avec délice 🥖')
+st.title('🍞 Falomy Boulanger🥖')
+st.text('Où la farine et le sucre dansent avec délice')
+ 
 
-
-st.header('Ventes et dépenses quotidiennes de boulangerie')
+#st.header('Ventes et dépenses quotidiennes de boulangerie')
 # This is to add Images as needed
 #import streamlit as st
 #st.image('sunrise.jpg', caption='Sunrise by the mountains')
