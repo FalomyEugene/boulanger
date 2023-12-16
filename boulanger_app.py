e08763aa46154c62ba1ef473c9741e93a527edb6
FalomyEugene <93395642+FalomyEugene@users.noreply.github.com>
2023-12-16 01:35:22 -0600

    Update boulanger_app.py
    check

--- boulanger_app.py
+++ boulanger_app.py
@@ -1,16 +1,27 @@
+import os
 import streamlit as st
 import pandas as pd
-import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import gspread
 
-# Adding a title and description
-st.title('🍞 Falomy Boulanger🥖')
-st.markdown('"Où la farine et le sucre dansent avec délice"')
+# Retrieve credentials from environment variables
+client_email = os.environ.get("GOOGLE_SHEETS_CLIENT_EMAIL")
+private_key = os.environ.get("GOOGLE_SHEETS_PRIVATE_KEY").replace('\\n', '\n')  # Replace escaped newlines
 
-# Google Sheets authentication
-scope = ['https://spreadsheets.google.com/feeds',
-         'https://www.googleapis.com/auth/drive']
-credentials = ServiceAccountCredentials.from_json_keyfile_name('path/to/your/credentials.json', scope)
+# Authenticate with Google Sheets
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+credentials = ServiceAccountCredentials.from_json_keyfile_dict({
+    "client_email": client_email,
+    "private_key": private_key,
+    "type": "service_account",
+    "project_id": "boulanger-408218",
+    "private_key_id": "a30fa345c1c99cceb7e6c184e2a7330fffdbbc0a",
+    "client_id": "109713103821114642089",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/boulanjesheet%40boulanger-408218.iam.gserviceaccount.com"
+}, scope)
 gc = gspread.authorize(credentials)
 
 # Open the Google Sheet
