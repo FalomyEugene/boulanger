ab9346dbbb751a6b2c1b63cbbf6eda23a68051cb
FalomyEugene <93395642+FalomyEugene@users.noreply.github.com>
2023-12-16 01:42:58 -0600

    Update boulanger_app.py
    Not sure why line 4 is not running

--- boulanger_app.py
+++ boulanger_app.py
@@ -1,7 +1,7 @@
 import os
 import streamlit as st
 import pandas as pd
-from oauth2client.service_account import ServiceAccountCredentials
+#from oauth2client.service_account import ServiceAccountCredentials
 import gspread
 
 # Retrieve credentials from environment variables
