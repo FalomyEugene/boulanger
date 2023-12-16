7157b765fcc1d30f951c830dc39eba9759b706a5
FalomyEugene <93395642+FalomyEugene@users.noreply.github.com>
2023-12-16 02:19:50 -0600

    Update boulanger_app.py
    try

--- boulanger_app.py
+++ boulanger_app.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 import pandas as pd
 import subprocess
-from gspread.service_account import ServiceAccountCredentials
+#from gspread.service_account import ServiceAccountCredentials
 import toml
 import gspread
 
