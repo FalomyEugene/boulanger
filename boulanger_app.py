c6305661f39f0ac125213ea3c206896d930a0c12
FalomyEugene <93395642+FalomyEugene@users.noreply.github.com>
2023-12-18 15:50:26 -0600

    Update boulanger_app.py

--- boulanger_app.py
+++ boulanger_app.py
@@ -1,7 +1,7 @@
 # example/st_app.py
 
 import streamlit as st
-import GSheetsConnection
+#from streamlit_gsheets import GSheetsConnection
 
 url = "https://docs.google.com/spreadsheets/d/1JDy9md2VZPz4JbYtRPJLs81_3jUK47nx6GYQjgU8qNY/edit?usp=sharing"
 
