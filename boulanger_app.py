36a16e70bee3738abc8fc44db6cb5254578923d6
FalomyEugene <93395642+FalomyEugene@users.noreply.github.com>
2023-12-18 15:56:00 -0600

    Update boulanger_app.py
    try

--- boulanger_app.py
+++ boulanger_app.py
@@ -1,11 +1,2 @@
-# example/st_app.py
-
-import streamlit as st
-#from streamlit_gsheets import GSheetsConnection
-
-url = "https://docs.google.com/spreadsheets/d/1JDy9md2VZPz4JbYtRPJLs81_3jUK47nx6GYQjgU8qNY/edit?usp=sharing"
-
-conn = st.experimental_connection("gsheets", type=GSheetsConnection)
-
-data = conn.read(spreadsheet=url, usecols=[0, 1])
-st.dataframe(data)
+# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
