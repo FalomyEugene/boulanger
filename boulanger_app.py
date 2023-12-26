6e05e90af3637fd8a8fdbfd500bd9aea43a6d9b4
FalomyEugene <93395642+FalomyEugene@users.noreply.github.com>
2023-12-26 11:16:54 -0600

    Update boulanger_app.py
    Code

--- boulanger_app.py
+++ boulanger_app.py
@@ -1,1 +1,27 @@
+import pandas as pd
+import gspread as gs
+from google.oauth2 import service_account
 
+
+SCOPES = [
+'https://www.googleapis.com/auth/spreadsheets',
+'https://www.googleapis.com/auth/drive'
+]
+SERVICE_ACCOUNT_FILE = r'C:\Users\Feugene\boulanjer cred.json'
+
+credentials = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+client = gs.authorize(credentials)
+
+
+sheet = client.open('Test1')
+edutech_data = sheet.get_worksheet(0)
+edutech_data = edutech_data.get_all_records()
+edutech_data
+
+edutech_df = pd.DataFrame.from_dict(edutech_data)
+edutech_df.head(3)
+print(edutech_df)
+Gtest.py
+Displaying boulanjer cred.json.
