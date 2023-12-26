83873c42675853e04e11d4901e17d6300257005f
FalomyEugene <93395642+FalomyEugene@users.noreply.github.com>
2023-12-26 17:06:40 -0600

    Update boulanger_app.py
    try

--- boulanger_app.py
+++ boulanger_app.py
@@ -27,7 +27,8 @@ st.write(edutech_df.head(3))
 # print(edutech_df)
 
 # ---- MAINPAGE ----
-st.title(":bar_chart: Falomy Boulangerie _ Rapport hebdomadaire ")
+# ---- MAINPAGE ----
+st.title(":bar_chart: Falomy Boulangerie _ Rapport hebdomadaire ", unsafe_allow_html=True)
 st.markdown("<h2 style='font-size:12px; display:inline-block;'> </h2>", unsafe_allow_html=True)
 
 
