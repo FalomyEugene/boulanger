e82182ed5bbb206e41359a54002b44e830fba223
FalomyEugene <93395642+FalomyEugene@users.noreply.github.com>
2023-12-26 17:26:04 -0600

    Update boulanger_app.py

--- boulanger_app.py
+++ boulanger_app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import gspread as gs
 from google.oauth2 import service_account
 import streamlit as st
+from datetime import datetime
 
 
 SCOPES = [
@@ -30,6 +31,14 @@ st.write(edutech_df.head(3))
 st.title(":bar_chart: Rapport - hebdomadaire")
 st.markdown("##")
 
+
+# Get the current date and time
+
+current_date_time = datetime.now()
+# Format the date as a string
+formatted_date = current_date_time.strftime("%Y-%m-%d %H:%M:%S")
+# Display the date in your Streamlit app
+st.write("Current Date and Time:", formatted_date)
 
 
 
