6e73bdee7ea5e8bfb885ff6e9e1b800b4fc91ed9
FalomyEugene <93395642+FalomyEugene@users.noreply.github.com>
2023-12-27 15:38:39 -0600

    Update boulanger_app.py

--- boulanger_app.py
+++ boulanger_app.py
@@ -18,6 +18,13 @@ client = gs.authorize(credentials)
 # Open the second worksheet
 worksheet = client.open('Test1').get_worksheet(1)
 
+# Get all records from the worksheet
+records = worksheet.get_all_records()
+
+# Convert records to a DataFrame
+df = pd.DataFrame.from_dict(records)
+st.write(df.head(3))
+
 # ---- MAINPAGE ----
 st.title(":bar_chart: Rapport - hebdomadaire")
 st.markdown("##")
@@ -25,24 +32,34 @@ st.markdown("##")
 # ---- SIDEBAR ----
 st.sidebar.header("Rapportez Ici:")
 
-# Add a form
-with st.sidebar.form("my_form"):
-    # Loop through each material and create an input field for its value
-    for mat in mat_options:
-        # Use st.number_input to display the value
-        value = st.number_input(f"Enter value for {mat}", key=mat, value=st.session_state.mat_values[mat])
-        st.session_state.mat_values[mat] = value
+# Get input data from user
+date = st.sidebar.date_input("Date", datetime.now())
+# Convert date to a string
+date_str = date.strftime("%Y-%m-%d")
 
-    # Display the current values on the sidebar
-    st.sidebar.write("Current values:")
-    for mat in mat_options:
-        st.sidebar.write(f"{mat}: {st.session_state.mat_values[mat]}")
+# Define the materials
+mat_options = ["Farine", "Mantegue", "Bois", "Gaz", "Sucre", "Ledvin", "Sel", "Excell", "Autre"]
 
-    # Add a form submit button
-    submitted = st.form_submit_button("Submit")
+# Initialize session state
+if "mat_values" not in st.session_state:
+    st.session_state.mat_values = {mat: 0 for mat in mat_options}
+
+# Loop through each material and create an input field for its value
+for mat in mat_options:
+    # Use st.number_input to display the value
+    value = st.sidebar.number_input(f"Enter value for {mat}", key=mat, value=st.session_state.mat_values[mat])
+    st.session_state.mat_values[mat] = value
+
+# Display the current values on the sidebar
+st.sidebar.write("Current values:")
+for mat in mat_options:
+    st.sidebar.write(f"{mat}: {st.session_state.mat_values[mat]}")
+
+# Add a custom submit button with on_click method
+submit_button = st.sidebar.button("Submit")
 
 # Use on_click method to set values to zero after submission
-if submitted:
+if submit_button:
     # Prepare data to be updated
     data_to_update = [date_str] + [st.session_state.mat_values.get(mat, 0) for mat in mat_options]
 
