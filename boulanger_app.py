405a91d440b9f6c677e59a55683665d0d5bd0802
FalomyEugene <93395642+FalomyEugene@users.noreply.github.com>
2023-12-26 18:25:18 -0600

    Update boulanger_app.py

--- boulanger_app.py
+++ boulanger_app.py
@@ -4,36 +4,30 @@ from google.oauth2 import service_account
 import streamlit as st
 from datetime import datetime
 
-
 SCOPES = [
-'https://www.googleapis.com/auth/spreadsheets',
-'https://www.googleapis.com/auth/drive'
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive'
 ]
 SERVICE_ACCOUNT_FILE = 'boulanjer cred.json'
 
 credentials = service_account.Credentials.from_service_account_file(
-        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 
 client = gs.authorize(credentials)
 
+# Open the second worksheet
+worksheet = client.open('Test1').get_worksheet(1)
 
-sheet = client.open('Test1')
-sheet = sheet.get_worksheet(1)
-sheet = sheet.get_all_records()
-# edutech_data
+# Get all records from the worksheet
+records = worksheet.get_all_records()
 
-sheet = pd.DataFrame.from_dict(sheet)
-st.write(sheet.head(3))
- #edutech_df.head(3)
-# print(edutech_df)
+# Convert records to a DataFrame
+df = pd.DataFrame.from_dict(records)
+st.write(df.head(3))
 
 # ---- MAINPAGE ----
 st.title(":bar_chart: Rapport - hebdomadaire")
 st.markdown("##")
-
-
-# Get the current date and time
-
 
 # ---- SIDEBAR ----
 st.sidebar.header("Rapportez Ici:")
@@ -58,7 +52,7 @@ for mat in selected_mat:
 # Prepare data to be updated
 data_to_update = [date] + [mat_values.get(mat, 0) for mat in mat_options[:-1]]
 
-# Update Google Sheet
-sheet.append_row(data_to_update)
-st.success("Data updated successfully.")
+# Update the worksheet with the new row
+worksheet.append_row(data_to_update)
 
+st.success("Data updated successfully.")
