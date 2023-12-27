2d0a22712dc98034347ba24aac3e1ebc78b9c8d7
FalomyEugene <93395642+FalomyEugene@users.noreply.github.com>
2023-12-27 17:04:38 -0600

    Update boulanger_app.py

--- boulanger_app.py
+++ boulanger_app.py
@@ -26,49 +26,21 @@ df = pd.DataFrame.from_dict(records)
 st.write(df.head(3))
 
 # ---- MAINPAGE ----
+
 st.title(":bar_chart: Rapport - hebdomadaire")
 st.markdown("##")
 
 # ---- SIDEBAR ----
-st.sidebar.header("Rapportez Ici:")
 
-# Get input data from user
-date = st.sidebar.date_input("Date", datetime.now())
-# Convert date to a string
-date_str = date.strftime("%Y-%m-%d")
+st.title("Link to Google Form in Streamlit App")
 
-# Define the materials
-mat_options = ["Farine", "Mantegue", "Bois", "Gaz", "Sucre", "Ledvin", "Sel", "Excell", "Autre"]
+# Replace 'YOUR_GOOGLE_FORM_URL' with the actual URL of your Google Form
+google_form_url = "https://docs.google.com/forms/d/e/1FAIpQLSfw0wN0hundqYfBytzI_P1KBG5MrTsdwlAMmd1wz41hd5T7xA/viewform"
 
-# Initialize session state
-if "mat_values" not in st.session_state:
-    st.session_state.mat_values = {mat: 0 for mat in mat_options}
+# Create a hyperlink to the Google Form
+st.markdown(f"[Open Google Form]({google_form_url})")
 
-# Loop through each material and create an input field for its value
-for mat in mat_options:
-    # Use st.number_input to display the value
-    value = st.sidebar.number_input(f"Enter value for {mat}", key=mat, value=st.session_state.mat_values[mat])
-    st.session_state.mat_values[mat] = value
+# Rest of your Streamlit app
+# ...
 
-# Display the current values on the sidebar
-st.sidebar.write("Current values:")
-#for mat in mat_options:
- #   st.sidebar.write(f"{mat}: {st.session_state.mat_values[mat]}")
 
-# Add a custom submit button with on_click method
-submit_button = st.sidebar.button("Submit")
-
-# Use on_click method to set values to zero after submission
-if submit_button:
-    # Prepare data to be updated
-    data_to_update = [date_str] + [st.session_state.mat_values.get(mat, 0) for mat in mat_options]
-
-    # Update the worksheet with the new row
-    worksheet.append_row(data_to_update)
-
-    st.success("Data updated successfully.")
-    # Set values to zero after submission
-    st.session_state.mat_values = {mat: 0 for mat in mat_options}
-    # Rerun the app after submission
-    st.experimental_rerun()
-    st.stop()
