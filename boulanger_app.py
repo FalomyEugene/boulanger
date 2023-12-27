ef7690e36333e5dd2033b6a9cc5c8b5a856488e1
FalomyEugene <93395642+FalomyEugene@users.noreply.github.com>
2023-12-27 15:38:00 -0600

    Update boulanger_app.py

--- boulanger_app.py
+++ boulanger_app.py
@@ -25,19 +25,7 @@ st.markdown("##")
 # ---- SIDEBAR ----
 st.sidebar.header("Rapportez Ici:")
 
-# Get input data from user
-date = st.sidebar.date_input("Date", datetime.now())
-# Convert date to a string
-date_str = date.strftime("%Y-%m-%d")
-
-# Define the materials
-mat_options = ["Farine", "Mantegue", "Bois", "Gaz", "Sucre", "Ledvin", "Sel", "Excell", "Autre"]
-
-# Initialize session state
-if "mat_values" not in st.session_state:
-    st.session_state.mat_values = {mat: 0 for mat in mat_options}
-
-# Use st.form to create a form context
+# Add a form
 with st.sidebar.form("my_form"):
     # Loop through each material and create an input field for its value
     for mat in mat_options:
@@ -45,11 +33,16 @@ with st.sidebar.form("my_form"):
         value = st.number_input(f"Enter value for {mat}", key=mat, value=st.session_state.mat_values[mat])
         st.session_state.mat_values[mat] = value
 
-   # Add a custom submit button with on_click method
-submit_button = st.sidebar.button("Submit")
+    # Display the current values on the sidebar
+    st.sidebar.write("Current values:")
+    for mat in mat_options:
+        st.sidebar.write(f"{mat}: {st.session_state.mat_values[mat]}")
+
+    # Add a form submit button
+    submitted = st.form_submit_button("Submit")
 
 # Use on_click method to set values to zero after submission
-if submit_button:
+if submitted:
     # Prepare data to be updated
     data_to_update = [date_str] + [st.session_state.mat_values.get(mat, 0) for mat in mat_options]
 
@@ -59,6 +52,5 @@ if submit_button:
     st.success("Data updated successfully.")
     # Set values to zero after submission
     st.session_state.mat_values = {mat: 0 for mat in mat_options}
-
     # Rerun the app after submission
     st.experimental_rerun()
