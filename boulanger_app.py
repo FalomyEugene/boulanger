dd7435e2ff6382e0f8f886d9e4c567cb9f647319
FalomyEugene <93395642+FalomyEugene@users.noreply.github.com>
2023-12-28 17:29:17 -0600

    Update boulanger_app.py

--- boulanger_app.py
+++ boulanger_app.py
@@ -63,8 +63,7 @@ left_column, middle_column, right_column = st.columns(3)
 with left_column:
     formatted_total_sales = f"HT {total_sales.sum():,}" if total_sales.dtype in ['int64', 'float64'] else total_sales.sum()
     st.subheader("Total Sales:")
-    #st.write(formatted_total_sales)
-    st.subheader("formatted_total_sales")
+    st.subheader(st.write(formatted_total_sales))
 
 st.markdown("""---""")
 
