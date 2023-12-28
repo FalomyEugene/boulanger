d9625dc3602ec82fe56a895541442cdd55e155b6
FalomyEugene <93395642+FalomyEugene@users.noreply.github.com>
2023-12-28 16:56:06 -0600

    Update boulanger_app.py

--- boulanger_app.py
+++ boulanger_app.py
@@ -3,6 +3,7 @@ import gspread as gs
 from google.oauth2 import service_account
 import streamlit as st
 from datetime import datetime
+
 st.set_page_config(page_title="Boulanger Rapport", page_icon=":bar_chart:", layout="wide")
 
 SCOPES = [
@@ -45,34 +46,25 @@ st.sidebar.markdown(f"[ Boulanger Rapport]({google_form_url})")
 # Add a date filter in the sidebar
 selected_date = st.sidebar.date_input("Select Date", datetime.now())
 
+# Convert the date column to datetime if it's stored as a string
+df['1. Date du compte rendu?'] = pd.to_datetime(df['1. Date du compte rendu?'])
+
 # Filter DataFrame based on the selected date
 filtered_df = df[df['1. Date du compte rendu?'].dt.date == selected_date.date()]
 
 # Display the filtered DataFrame
 st.write(filtered_df)
 
-
-#st.sidebar.text("This is additional content in the sidebar.")
-
-
 # TOP KPI's
 numeric_columns = df.select_dtypes(include='number').columns
 total_sales = df[numeric_columns].sum(axis=1)
 
-
 left_column, middle_column, right_column = st.columns(3)
 with left_column:
-    # Format total_sales only if it's a numeric value, otherwise display as is
     formatted_total_sales = f"US $ {total_sales.sum():,}" if total_sales.dtype in ['int64', 'float64'] else total_sales.sum()
-
-    # Display the formatted total sales
     st.subheader(f"Total Sales: {formatted_total_sales}")
 
-   # st.subheader("Total Sales:")
-   # st.subheader(f"US $ {total_sales:,}")
-
 st.markdown("""---""")
-
 
 # ---- HIDE STREAMLIT STYLE ----
 hide_st_style = """
