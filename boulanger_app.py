a3b93e114ce24afb60bc26483c5dd669a30a2eeb
FalomyEugene <93395642+FalomyEugene@users.noreply.github.com>
2023-12-28 16:39:19 -0600

    Update boulanger_app.py

--- boulanger_app.py
+++ boulanger_app.py
@@ -46,6 +46,28 @@ st.sidebar.markdown(f"[ Boulanger Rapport]({google_form_url})")
 #st.sidebar.text("This is additional content in the sidebar.")
 
 
+# TOP KPI's
+total_sales= df.iloc[:, 2:-1].sum(axis=1)
+#total_sales = int(df_selection[""].sum())
+#average_rating = round(df_selection["Rating"].mean(), 1)
+#star_rating = ":star:" * int(round(average_rating, 0))
+#average_sale_by_transaction = round(df_selection["Total"].mean(), 2)
+
+
+left_column, middle_column, right_column = st.columns(3)
+with left_column:
+    st.subheader("Total Sales:")
+    st.subheader(f"US $ {total_sales:,}")
+#with middle_column:
+  #  st.subheader("Average Rating:")
+  #  st.subheader(f"{average_rating} {star_rating}")
+#with right_column:
+ #   st.subheader("Average Sales Per Transaction:")
+  #  st.subheader(f"US $ {average_sale_by_transaction}")
+
+st.markdown("""---""")
+
+
 # ---- HIDE STREAMLIT STYLE ----
 hide_st_style = """
             <style>
